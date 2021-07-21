8ba2e5c8b9ce957eba4d5173a5c641ae09908b40
Christine Jeon <csjeon28@gmail.com>
2021-07-21 12:33:29 -0400

    added item and user ids

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -8,7 +8,9 @@ end
 
 Item.create(name: Faker::House.furniture, price: Faker::Commerce.price)
 
-item_id = Item.all.map{|user| user.id}
+item_ids = Item.all.map{|item| item.id}
+user_ids = User.all.map{|user| user.id}
 
-Board.create(name: Faker::Commerce.department)
-
+10.times do
+    Board.create(user_id: rand(user_ids.first..user_ids.last), item_id: rand(item_ids.first..item_ids.last))
+end
