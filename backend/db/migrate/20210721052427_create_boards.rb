e7fc963a19fb811201b9f7aaa05940c3bda21c11
Christine Jeon <csjeon28@gmail.com>
2021-07-22 13:29:01 -0400

    change description to text from string

--- backend/db/migrate/20210721052427_create_boards.rb
+++ backend/db/migrate/20210721052427_create_boards.rb
@@ -1,6 +1,8 @@
 class CreateBoards < ActiveRecord::Migration[5.2]
   def change
     create_table :boards do |t|
+      t.string :title
+      t.text :description
       t.integer :item_id
       t.integer :user_id
       
