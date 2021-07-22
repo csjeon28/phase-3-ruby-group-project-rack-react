d5a952422cf3c07304c2473e0c96071762b05261
Christine Jeon <csjeon28@gmail.com>
2021-07-22 13:27:13 -0400

    added image url and description to table

--- backend/db/migrate/20210721052402_create_items.rb
+++ backend/db/migrate/20210721052402_create_items.rb
@@ -1,8 +1,16 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string :name
+      t.string :title
+      t.text :description
+      t.text :image_url
       t.integer :price
+      t.integer :user_id
+      t.integer :board_id
+
+      t.timestamps
     end
+    # add_index :items, :user_id
+    # add_index :items, :board_id
   end
 end
