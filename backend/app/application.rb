06018e104a65b068a425414a24d8d39937dbd752
Christine Jeon <csjeon28@gmail.com>
2021-07-28 23:28:57 -0400

    deleted commented out code

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -7,79 +7,11 @@ class Application
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
     
-    # GET/POST user request
-#     elsif req.path.match(/users/)
-#       if req.env["REQUEST_METHOD"] == "GET"
-#           if req.path.split("/users").length == 0
-#             users = User.all
-#             return [200, {'Content-Type' => 'application/json'}, [users.to_json]]
-#           end
-#         end
-#       if req.env["REQUEST_METHOD"] == "POST"
-#         form_data = JSON.parse(req.body.read)
-#         new_user = User.create(username: form_data["username"])
-#         return [200, {'Content-Type' => 'application/json'}, [new_user.to_json]]
-#       end
-
-#     # GET/POST/PATCH/DELETE item request
-#     elsif req.path.match(/wishes/)
-#       if req.env["REQUEST_METHOD"] == "GET"
-#           if req.path.split("/wishes").length == 0
-#           wishes = Wish.all
-#           return [200, {'Content-Type' => 'application/json'}, [wishes.to_json]]
-#           end
-#         end
-#       if req.env["REQUEST_METHOD"] == "POST"
-#         form_data = JSON.parse(req.body.read)
-#         new_wish = Wish.create(title: form_data["title"], description: form_data["description"])
-#         return [200, {'Content-Type' => 'application/json'}, [new_wish.to_json]]
-#       end
-#       if req.env["REQUEST_METHOD"] == "DELETE"
-#         wishes = Wish.all.last.delete
-#         return [200, {'Content-Type' => 'application/json'}, [wishes.to_json]]
-#       end
-#       # if req.env["REQUEST_METHOD"] == "PATCH"
-#       #   wish = Wish.update
-#       #   return [200, {'Content-Type' => 'application/json'}, [wish.to_json]]
-#       # end
-
-#     # GET/POST/PATCH/DELETE board request
-#     elsif req.path.match(/boards/)
-#       if req.env["REQUEST_METHOD"] == "GET"
-#         if req.path.split("/boards").length == 0
-#           boards = Board.all
-#           return [200, {'Content-Type' => 'application/json'}, [boards.to_json]]
-#         end
-#       end
-#       if req.env["REQUEST_METHOD"] == "POST"
-#         form_data = JSON.parse(req.body.read)
-#         new_board = Board.create(name: form_data["name"])
-#         return [200, {'Content-Type' => 'application/json'}, [new_board.to_json]]
-#       end
-#       if req.env["REQUEST_METHOD"] == "DELETE"
-#         boards = Board.all.last.delete
-#         return [200, {'Content-Type' => 'application/json'}, [boards.to_json]]
-#       end
-#       if req.env["REQUEST_METHOD"] == "PATCH"
-#         boards = Board.update
-#         return [200, {'Content-Type' => 'application/json'}, [boards.to_json]]
-#       end
-
-#     else
-#       return [400, {'Content-Type' => 'application/json'}, [ {:error => "Path Not Found"}.to_json]]
-#     end
-    
-#     resp.finish
-#   end
-# end
-
-
-
+    # GET/POST/DELETE user request
     elsif req.path.match(/users/) && req.get?
       users = User.all.map do |user| {username: user.username}
       end
       return [200, { 'Content-Type' => 'application/json' }, [ users.to_json]] 
-    # POST user request
     elsif req.path.match(/users/) && req.post?
       data = JSON.parse(req.body.read)
       userExists = User.find_by(username: data["username"])
@@ -95,39 +27,38 @@ class Application
       name = req.params["q"]
       user = User.find_by(:name => name)
       return [200, { "Content-Type" => "application/json" }, [{:user => user, :userWishes => user.wishes, :userBoards => user.boards}.to_json]]
-
+    
+      # GET/POST/PUT/DELETE wish request
     elsif req.path.match(/wishes/) && req.get?
-      wishes = Wish.all.map do |wish| {
-        title: wish.title, 
-        description: wish.description, 
-        board_id: wish.board_id,
-        user_id: wish.user_id
-      }
+      wishes = Wish.all.map do |wish| {title: wish.title, description: wish.description}
       end
-      return [200, { 'Content-Type' => 'application/json' }, [{:data => {:wish => wishes}}.to_json]]
+      return [200, { 'Content-Type' => 'application/json' }, [wishes.to_json]]
     elsif req.path.match(/wishes/) && req.post?
       data = JSON.parse(req.body.read)
       wish = Wish.create(data)
         return [200, { 'Content-Type' => 'application/json' }, [{:data => {:message => "Wish created", :wish => wish}}.to_json]]
-    elsif req.path.match(/items/) && req.put?
+    elsif req.path.match(/wishes/) && req.put?
       wish = Wish.update(data)
         return [200, { 'Content-Type' => 'application/json' }, [{:data => {:message => "Wish updated", :wish => wish}}.to_json]]
-    elsif req.delete?
-      id = req.path.split("/courses/").last
-      Wish.find(id).delete
-        return [200, { 'Content-Type' => 'application/json' }, [ {:message => "Wish deleted!"}.to_json ]]
+    # elsif req.delete?
+    elsif req.path.match(/wishes/) && req.delete?
+      id = req.path.split("/wishes/").last
+      wish = Wish.find(id).delete
+        return [200, { 'Content-Type' => 'application/json' }, [{:data => {:message => "Wish deleted!", :wish => wish}}.to_json ]]
 
+     # GET/POST/DELETE board request
     elsif req.path.match(/boards/) && req.get?
-      boards = Board.all.map do |board| {
-        name: board.name, 
-        wish_id: board.wish_id
-      }
+      boards = Board.all.map do |board| {name: board.name}
       end
-      return [200, { 'Content-Type' => 'application/json' }, [ {:data => {:board => boards}}.to_json]]
+      return [200, { 'Content-Type' => 'application/json' }, [ boards.to_json]]
     elsif req.path.match(/boards/) && req.post?
       data = JSON.parse(req.body.read)
       board = Board.create(data)
         return [200, { 'Content-Type' => 'application/json' }, [ {:data => {:message => "Board created", :board => board}}.to_json]]
+    elsif req.path.match(/boards/) && req.delete?
+      id = req.path.split("/boards/").last
+      board = Board.find(id).delete
+        return [200, { 'Content-Type' => 'application/json' }, [{:data => {:message => "Board deleted!", :board => board}}.to_json ]]
 
     else
       return [400, {'Content-Type' => 'application/json'}, [ {:error => "Path Not Found"}.to_json]]
