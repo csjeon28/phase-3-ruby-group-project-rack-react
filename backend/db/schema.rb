a9ba8f1093a50bfadc46e1a84f234fed9978c041
Christine Jeon <csjeon28@gmail.com>
2021-07-28 23:28:18 -0400

    updated seed data

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_07_26_014608) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.integer "board_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
