d1b137e8bd49099572efa30456de608aafa42886
Christine Jeon <csjeon28@gmail.com>
2021-08-05 14:10:26 -0400

    fixed seed data to randomize id numbers to match

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -8,18 +8,19 @@ User.create(username: Faker::Name.name)
 
 puts "Seeding users..."
 
-Wish.create(title: "Accent Chairs", description: "For the living room", user_id: 1, board_id: 2)
-Wish.create(title: "Lamps", description: "For the bedroom", user_id: 2, board_id: 4)
-Wish.create(title: Faker::Coffee.blend_name, description: "Coffee blends I like", user_id: 3, board_id: 5)
-
+20.times do
+Wish.create(title: "Accent Chairs", description: "For the living room", user_id: rand(1..3), board_id: rand(1..10))
+Wish.create(title: "Lamps", description: "For the bedroom", user_id: rand(1..3), board_id: rand(1..10))
+Wish.create(title: Faker::Coffee.blend_name, description: "Coffee blends I like", user_id: rand(1..3), board_id: rand(1..10))
+end
 
 puts "Seeding wishes..."
 
-Board.create(name: "Interior Design", wish_id: 1, user_id: 1)
-Board.create(name: "Must-Have List", wish_id: 2, user_id: 2)
-Board.create(name: "Drinks", wish_id: 3, user_id: 2)
-Board.create(name: "Tech Toys", wish_id: 2, user_id: 3)
-Board.create(name: "Sports", wish_id: 1, user_id: 3)
+Board.create(name: "Interior Design", wish_id: rand(1..10), user_id: rand(1..3))
+Board.create(name: "Must-Have List", wish_id: rand(1..10), user_id: rand(1..3))
+Board.create(name: "Drinks", wish_id: rand(1..10), user_id: rand(1..3))
+Board.create(name: "Tech Toys", wish_id: rand(1..10), user_id: rand(1..3))
+Board.create(name: "Sports", wish_id: rand(1..10), user_id: rand(1..3))
 
 puts "Seeding boards..."
 
