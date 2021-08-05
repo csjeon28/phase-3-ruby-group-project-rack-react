550f3796868a75045fcdbb7b917d18529cb36689
Christine Jeon <csjeon28@gmail.com>
2021-08-05 03:35:48 -0400

    fixed delete request for boards

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -8,43 +8,37 @@ class Application
 
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
-#<-----------USERS GET REQUEST------------->
-    # elsif req.path.match(/users/) && req.get?
-    #   users = User.all.map do |user| {username: user.username}
-    #   end
-    #   return [200, { 'Content-Type' => 'application/json' }, [ {:data => {:user => users}}.to_json]] 
-#<-----------USERS POST REQUEST------------->
+#<---------------USERS GET/POST REQUEST--------------->
     elsif req.path.match(/users/) && req.post?
       data = JSON.parse(req.body.read)
       userExists = User.find_by(username: data["username"])
       if userExists
         return [200, { 'Content-Type' => 'application/json' }, 
-        [ {:message => "Sign-in Completed"}.to_json]]
+        [ {:message => "User Exists!", :user => userExists}.to_json]]
       else    
-        User.create(username: data["username"])
-        create_user = {username: data["username"]}
+        new_user = User.create(username: data["username"])
         return [200, { 'Content-Type' => 'application/json' }, 
-        [ { :data => {:message => "Sign-in Completed", :user => create_user}}.to_json]]
+        [ {:message => "Sign-in Completed", :user => new_user}.to_json]]
       end
-    elsif req.path.match(/users/)
+    elsif req.path.match(/users/) 
       username = req.params['q']
       user = User.find_by(:username => username)
       return [200, { "Content-Type" => "application/json" }, [{:user => user, :userBoards => user.boards}.to_json]]
-#<-----------BOARDS GET REQUEST------------->
+#<----------------BOARDS GET REQUEST----------------->
     elsif req.path.match(/boards/) && req.get?
       boards = Board.all.map do |board| {id: board.id, name: board.name, user: board.user_id}
       end
-      return [200, { 'Content-Type' => 'application/json' }, [ {:data => {:board => boards}}.to_json]]
-#<-----------BOARDS POST REQUEST------------->
+    return [200, { 'Content-Type' => 'application/json' }, [ {:board => boards}.to_json]]
+#<----------------BOARDS POST REQUEST---------------->
     elsif req.path.match(/boards/) && req.post?
       data = JSON.parse(req.body.read)
       username = User.find_by(username: data["username"])
       board = Board.create(name: data["name"], user_id: username.id)
-      return [200, { 'Content-Type' => 'application/json' }, 
-      [ {:board => board}.to_json]]
-#<-----------BOARDS DELETE REQUEST------------->
+      return [200, { 'Content-Type' => 'application/json' }, [ {:board => board}.to_json]]
+#<---------------BOARDS DELETE REQUEST---------------->
     elsif req.delete?
       id = req.path.split("/boards/").last
+      # binding.pry
       board = Board.find(id).delete
         return [200, { 'Content-Type' => 'application/json' }, [{:message => "Board deleted!", :board => board}.to_json ]]
 
